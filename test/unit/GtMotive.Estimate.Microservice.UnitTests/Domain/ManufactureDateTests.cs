6a5473ba591e86ad70c56cc38f1e88ace8a0b092
Antonio Candal <acandaldev@gmail.com>
2026-05-28 17:08:19 +0200

    Add unit tests and docker-compose for MongoDB

---
+++ test/unit/GtMotive.Estimate.Microservice.UnitTests/Domain/ManufactureDateTests.cs
@@ -0,0 +1,35 @@
+using System;
+using GtMotive.Estimate.Microservice.Domain.Exceptions;
+using GtMotive.Estimate.Microservice.Domain.ValueObjects;
+using Xunit;
+
+namespace GtMotive.Estimate.Microservice.UnitTests.Domain
+{
+    public class ManufactureDateTests
+    {
+        [Fact]
+        public void ManufactureDateWithValidDateShouldCreate()
+        {
+            var date = DateTime.UtcNow.AddYears(-2);
+            var manufactureDate = new ManufactureDate(date);
+
+            Assert.Equal(date.Date, manufactureDate.Value);
+        }
+
+        [Fact]
+        public void ManufactureDateOlderThanFiveYearsShouldThrow()
+        {
+            var tooOld = DateTime.UtcNow.AddYears(-6);
+
+            Assert.Throws<VehicleTooOldException>(() => new ManufactureDate(tooOld));
+        }
+
+        [Fact]
+        public void ManufactureDateInFutureShouldThrow()
+        {
+            var future = DateTime.UtcNow.AddDays(1);
+
+            Assert.Throws<ArgumentException>(() => new ManufactureDate(future));
+        }
+    }
+}
